3e8e6cd5c0b4f09448ac84fcf7526a26188b0be0
josheinhorn <josh.einhorn@gmail.com>
2015-10-05 15:18:40 -0400

    Removed code referencing old socket

--- src/StatsdClient/StatsdUDP.cs
+++ src/StatsdClient/StatsdUDP.cs
@@ -8,19 +8,16 @@ namespace StatsdClient
 {
     public class StatsdUDP : MetricsSenderBase, IStatsdUDP, IDisposable
     {
-        private readonly Socket socket;
         private readonly UdpClient udp;
         private bool _disposed;
         public StatsdUDP(string host, int port, int maxPacketSize = MetricsConfig.DefaultMaxPacketSize)
             : base(host, port, maxPacketSize)
         {
-            //socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            udp = new UdpClient(); // should we specifically use the port provided? depends on firewall/AWS SG
+            udp = new UdpClient(); // no need to specify a Local Port since we don't need to receive responses
         }
         protected override async Task SendCommand(byte[] command)
         {
             await udp.SendAsync(command, command.Length, IPEndpoint);
-            //socket.SendTo(command, command.Length, SocketFlags.None, IPEndpoint);
         }
 
         //reference : https://lostechies.com/chrispatterson/2012/11/29/idisposable-done-right/
@@ -41,18 +38,6 @@ namespace StatsdClient
 
             if (disposing)
             {
-                if (socket != null)
-                {
-                    try
-                    {
-                        socket.Close();
-                    }
-                    catch (Exception)
-                    {
-                        //Swallow since we are not using a logger, should we add LibLog and start logging??
-                    }
-                    
-                }
                 if (udp != null)
                 {
                     try { udp.Close(); }
